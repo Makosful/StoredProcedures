4a72957c7f9c04e700d0551bb0b1746f730be993
Axl <32297912+Makosful@users.noreply.github.com>
2021-03-03 07:34:06 +0000

    Update usp_GetAllDepartments.sql

--- sql/usp_GetAllDepartments.sql
+++ sql/usp_GetAllDepartments.sql
@@ -1,7 +1,6 @@
 CREATE PROCEDURE usp_GetAllDepartments
 AS
 BEGIN
-	SELECT	COUNT(e.SSN) AS EmployeeCount, e.Dno AS DNumber
-	FROM	Employee AS e 
-	GROUP BY e.Dno;
+	SELECT	d.Employees
+	FROM	Department AS d;
 END
